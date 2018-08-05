27031ee575ef249177c684b878f5ee10513ffd22
Radwa Ahmed Mohsen <radwa@gfxway.com>
2018-08-05 23:50:36 +0200

    the menu suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,7 +59,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+      describe('The Menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -71,8 +71,8 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
-    /* TODO: Write a new test suite named "Initial Entries" */
+      });
+          /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
